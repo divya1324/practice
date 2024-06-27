6e6107762a230cef1cbdc8cac389b116703b1cd2
divya1324 <dt8411894@gmail.com>
2024-06-27 20:34:14 +0530

    data variables task completed

---
+++ task3.js
@@ -0,0 +1,19 @@
+var str = "Hello"
+const score = 100
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail = undefined
+
+const movies = ["kalki", "ram siya", "bahubali"];
+var obj = { key: "value", age: 30 };
+function greet(name) {
+    return "Hello, " + name + "!";
+}
+
+console.log("str:", str, typeof str);
+console.log("score:", score, typeof score);
+console.log("isLoggedIn:", isLoggedIn, typeof isLoggedInd);
+console.log("userEmail:", userEmail, typeof userEmail);
+console.log("heros:", movies, typeof movies);
+console.log("obj:", obj, typeof obj);
+console.log("greet:", greet, typeof greet);
